b29ed01ab0f95e2be407b2d61ae58f311b4f6b0a
Caaronbll <aaronbll46@gmail.com>
2023-06-08 12:01:43 -0700

    q3 making the basemodel class

--- models/base_model.py
+++ models/base_model.py
@@ -5,15 +5,30 @@ defines all common attributes
 /methods for other classes
 """
 import uuid
-import datetime
+from datetime import datetime
+
 
 class BaseModel():
     """Declaration of BaseModel class"""
-    def __init__(self, id, created_at, updated_at):
-        self.__id = id
-        self.__created_at = created_at
-        self.__updated_at = updated_at
 
-    def id(self):
-        self.__id = str(uuid.uuid4())
-        return self.__id
+    def __init__(self):
+        """BaseModel constructor"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """returns the string given"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """saves the instance"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns dictionary of keys/values"""
+        new_dict = dict(self.__dict__)
+        new_dict["__class__"] = self.__class__.__name__
+        timeFormat = "%Y-%m-%dT%H:%M:%S.%f"
+        new_dict["created_at"] = self.created_at.strftime(timeFormat)
+        new_dict["updated_at"] = self.updated_at.strftime(timeFormat)

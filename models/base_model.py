3e0b4874c540277528af7e5b24813f517bdcb3a0
pretzelogical <yingyangyo266@gmail.com>
2023-06-10 12:15:45 -0500

    Add object saving (T5)

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from models import storage
 """
 Class BaseModel that defines all common attributes and
 methods for other classes
@@ -16,10 +17,12 @@ class BaseModel:
         if kwargs:
             self.id = kwargs['id']
             self.created_at = datetime.fromisoformat(kwargs['created_at'])
+            self.updated_at = datetime.now()
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """returns the string given"""
@@ -28,10 +31,11 @@ class BaseModel:
     def save(self):
         """saves the instance"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns dictionary of keys/values"""
-        new_dict = self.__dict__
+        new_dict = self.__dict__.copy()
         new_dict["__class__"] = self.__class__.__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import json
+import os
 """
 Write a class FileStorage
 that serializes instances
@@ -8,10 +10,9 @@ JSON file to instances
 
 
 class FileStorage:
-    def __init__(self, __file_path, __objects, ):
-        self.__file_path = __file_path
-        self.__objects = __objects
-
+    """Serializes and deserializes instances"""
+    __file_path = "saved.json"
+    __objects = {}
 
     def all(self):
         """returns dictionary __objects"""
@@ -19,4 +20,19 @@ class FileStorage:
 
     def new(self, obj):
         """adds an obj and its key in the dict"""
-        self.__objects[f"{obj}"]
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+
+    def save(self):
+        """Serializes __objects to __file_path"""
+        with open(self.__file_path, "w") as f:
+            out = {}
+            for key, value in self.__objects.items():
+                out[key] = value.to_dict()
+            json.dump(out, f)
+
+    def reload(self):
+        """Deserializes __file_path to __objects"""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                self.__objects = json.load(f)
+

b2dcb2cd0be74b82d66d049fca227ea08b6ea2c0
Caaronbll <aaronbll46@gmail.com>
2023-06-10 22:05:00 -0700

    taking out self in filestorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,23 +16,23 @@ class FileStorage:
 
     def all(self):
         """returns dictionary __objects"""
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """adds an obj and its key in the dict"""
-        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        FileStorage.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
 
     def save(self):
         """Serializes __objects to __file_path"""
-        with open(self.__file_path, "w") as f:
+        with open(FileStorage.__file_path, "w") as f:
             out = {}
-            for key, value in self.__objects.items():
+            for key, value in FileStorage.__objects.items():
                 out[key] = value.to_dict()
             json.dump(out, f)
 
     def reload(self):
         """Deserializes __file_path to __objects"""
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, "r") as f:
-                self.__objects = json.load(f)
+        if os.path.exists(FileStorage.__file_path):
+            with open(FileStorage.__file_path, "r") as f:
+                FileStorage.__objects = json.load(f)
 

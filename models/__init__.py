2ab388fd979c832b097f9538d3e23e6282ec46c8
Caaronbll <aaronbll46@gmail.com>
2023-06-10 21:58:11 -0700

    added for question 5

---
+++ models/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+import models.engine.file_storage as fs
+
+storage = fs.FileStorage()
+storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+from models import storage
 """
 Class BaseModel that defines all common attributes and
 methods for other classes
@@ -21,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """returns the string given"""
@@ -29,6 +31,7 @@ class BaseModel:
     def save(self):
         """saves the instance"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns dictionary of keys/values"""

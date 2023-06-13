3a99f80a1838f9742eaeb01414ddfbd437a20850
Caaronbll <aaronbll46@gmail.com>
2023-06-13 13:35:11 -0700

    testing again

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -22,10 +22,18 @@ class TestUser(unittest.TestCase):
     def test_methods(self):
         """tests if attributes are strings"""
         this_user = User()
+        this_user.email = "test@gmail.com"
+        this_user.password = "1234567"
+        this_user.first_name = "Derek"
+        this_user.last_name - "Webb"
         self.assertTrue(type(this_user.email) == str)
         self.assertTrue(type(this_user.password) == str)
         self.assertTrue(type(this_user.first_name) == str)
         self.assertTrue(type(this_user.last_name) == str)
+        self.assertIsNotNone(this_user.email)
+        self.assertIsNotNone(this_user.password)
+        self.assertIsNotNone(this_user.first_name)
+        self.assertIsNotNone(this_user.last_name)
 
 if __name__ == '__main__':
     unittest.main()

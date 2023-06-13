5496f3979b967ffc050ef2d5c2eb22d1f5f5d53d
pretzelogical <yingyangyo266@gmail.com>
2023-06-12 22:03:01 -0500

    add do_show

--- console.py
+++ console.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python3
 from models.base_model import BaseModel
+from models import storage
 import cmd
 """Command line interface for AirBnB clone"""
 
 class_list = {'BaseModel': BaseModel}
+error_messages = {'classMiss': '** class name missing **',
+                  'classNotExist': "** class doesn't exist **",
+                  'idMiss': "** instance id missing **",
+                  'idNoInstance': "** no instance found **"}
 
 class HBNBCommand(cmd.Cmd):
     """Command line interface for AirBnB clone"""
     prompt = '(hbnb) '
 
     def do_create(self, arg):
-        """Creates a new BaseModel instance, saves it and prints the id"""
+        """Creates a new BaseModel instance, saves it and prints the id
+        ex: create BaseModel
+        """
         if not arg:
-            print('** class name missing **')
+            print(error_messages['classMiss'])
             return
         for key, value in class_list.items():
             if key == arg:
@@ -20,12 +27,28 @@ class HBNBCommand(cmd.Cmd):
                 new_obj.save()
                 print(new_obj.id)
                 return
-        print("** class doesn't exist **")
+        print(error_messages['classNotExist'])
 
-    def do_show_arg(self, arg):
-        print(arg)
+    def do_show(self, arg):
+        """Show an instance ex: show <obj> <id>"""
         if not arg:
-            print('no arg')
+            print(error_messages['classMiss'])
+            return
+        args = self.split_arg(arg)
+        name = args[0]
+        objects = storage.all()
+
+        if name not in class_list.keys():
+            print(error_messages['classNotExist'])
+            return
+
+        if len(args) != 2:
+            print(error_messages['idMiss'])
+            return
+        id = args[1]
+
+        try: print(objects[f'{name}.{id}'])
+        except: print(error_messages['idNoInstance']); return
 
     def do_quit(self, arg):
         """Quit hbnb shell"""
